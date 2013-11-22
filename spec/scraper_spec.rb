f689ec52b5ba8e17dac5e5c52bf30de48db9ce91
Sara Gorecki <sgorecki@gmail.com>
2013-11-21 23:50:34 -0500

    add a test to test html method

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,7 +1,15 @@
-require "../lib/scraper.rb"
+require "../lib/scraper"
 
 describe Scraper, "#get_species" do
 	it "should look at a pokemon page on bulbapedia and return the name" do
 		expect(Scraper.new("http://bulbapedia.bulbagarden.net/wiki/Girafarig").get_species).to eq("Girafarig")
 	end
 end
+
+describe Scraper, "#html" do
+	it "should return an HTML document" do
+		expect(Scraper.new("http://bulbapedia.bulbgarden.net").html).to be_a_kind_of(Document)
+	end	
+end
+
+
